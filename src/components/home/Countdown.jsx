42d2dae26be768de899b038cb051f494dd8ee5f7
Igor Martyniuk <martyniuk.igor@rexsoftinc.co>
2024-07-27 15:53:34 +0300

    fix

--- src/components/home/Countdown.jsx
+++ src/components/home/Countdown.jsx
@@ -8,8 +8,12 @@ function Countdown({ time, label }) {
     <div className="w-1/4 flex flex-col">
       <div className="w-full text-center flex justify-center font-libreBodoni">
         <div className="flex-grow flex justify-center">
-          <div className="w-[24px] fade" key={formated[0]}>{formated[0]}</div>
-          <div className="w-[24px] fade" key={formated[1]}>{formated[1]}</div>
+          <div className="w-[24px] fade" key={label + "1" + formated[0]}>
+            {formated[0]}
+          </div>
+          <div className="w-[24px] fade" key={label + "2" + formated[1]}>
+            {formated[1]}
+          </div>
         </div>
       </div>
 
